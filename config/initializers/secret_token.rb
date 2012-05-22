dfc6c3a0412cc8c581e144e8533a934259f176eb
Joshua Clayton <jclayton@thoughtbot.com>
2012-05-22 09:50:25 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hashtag::Application.config.secret_token = '57f457f5b2ee936000d0df17183a5080c82277d192e84981e5054944c73b811692cb5c717d5cc7b92497ca1bfcd11ece3902965676270dfc36d64938ce48be49'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Hashtag::Application.config.session_store :cookie_store, key: '_hashtag_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Hashtag::Application.config.session_store :active_record_store
