f1eaa394b2c1208d6a38769f4f0345173ecf8c70
Joshua Clayton <jclayton@thoughtbot.com>
2012-09-18 10:07:38 -0700

    Refactor

--- spec/integration/view_homepage_spec.rb
+++ spec/integration/view_homepage_spec.rb
@@ -2,7 +2,15 @@ require 'spec_helper'
 
 feature 'View the homepage' do
   scenario 'gather information about the site' do
+    visit_homepage
+    page_should_be_titled 'Hashtag'
+  end
+
+  def visit_homepage
     visit root_path
-    page.should have_css('title', text: 'Hashtag')
+  end
+
+  def page_should_be_titled(text)
+    page.should have_css('title', text: text)
   end
 end
