1d8c374917b20b18cf9e138289b2b081daaa1486
Joshua Clayton <jclayton@thoughtbot.com>
2013-04-09 13:23:20 -0400

    Refactor searching out into its own method

--- spec/features/guest_searches_for_hashtags_spec.rb
+++ spec/features/guest_searches_for_hashtags_spec.rb
@@ -2,20 +2,22 @@ require 'spec_helper'
 
 feature 'Guest searches for hashtags' do
   scenario 'without a hashtag' do
-    visit root_path
-    fill_in 'Search', with: 'rails'
-    click_on 'Submit'
+    search_for 'rails'
 
     expect(current_path).to eq '/searches/rails'
     expect(page).to have_css 'li', text: /#rails/i, count: 15
   end
 
   scenario 'with a hashtag' do
-    visit root_path
-    fill_in 'Search', with: '#rails'
-    click_on 'Submit'
+    search_for '#rails'
 
     expect(current_path).to eq '/searches/rails'
     expect(page).to have_css 'li', text: /#rails/i, count: 15
+  end
+
+  def search_for(term)
+    visit root_path
+    fill_in 'Search', with: term
+    click_on 'Submit'
   end
 end
