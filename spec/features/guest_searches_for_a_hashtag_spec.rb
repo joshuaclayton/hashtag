9aa6610ac72a01c7c5dcfd397486cd3b49657962
Joshua Clayton <jclayton@thoughtbot.com>
2013-05-21 12:58:00 -0400

    Minor refactor

--- spec/features/guest_searches_for_a_hashtag_spec.rb
+++ spec/features/guest_searches_for_a_hashtag_spec.rb
@@ -2,32 +2,32 @@ require 'spec_helper'
 
 feature 'Guest searches for a hashtag' do
   scenario 'displays search results' do
-    visit root_path
-    fill_in 'Search', with: '#rails'
-    click_on 'Submit'
+    search_for '#rails'
 
     expect(current_path).to eq '/searches/rails'
     expect(page).to have_css 'li', text: /#rails/i, count: 15
   end
 
   scenario 'searches without a hashtag' do
-    visit root_path
-    fill_in 'Search', with: 'rails'
-    click_on 'Submit'
+    search_for 'rails'
 
     expect(current_path).to eq '/searches/rails'
     expect(page).to have_css 'li', text: /#rails/i, count: 15
   end
 
   scenario 'updates search results' do
-    visit root_path
-    fill_in 'Search', with: '#rails'
-    click_on 'Submit'
+    search_for '#rails'
 
     fill_in 'Search', with: '#ruby'
     click_on 'Submit'
 
     expect(current_path).to eq '/searches/ruby'
     expect(page).to have_css 'li', text: /#ruby/i, count: 15
+  end
+
+  def search_for(term)
+    visit root_path
+    fill_in 'Search', with: term
+    click_on 'Submit'
   end
 end
